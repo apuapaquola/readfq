fac68316ec03203180760b4993e939a20f4d5af5
Apua Paquola <apaquola@salk.edu>
2013-06-04 10:21:20 -0700

    Added docstring. Changed l[:-1] to l.rstrip(). Changed the name field behavior. It now now has the whole title line (except for the '@' or '>' character) of the fq or fa record, not only the string before the first space character, as before.

--- readfq.py
+++ readfq.py
@@ -1,26 +1,42 @@
-def readfq(fp): # this is a generator function
+def readfq(fp):
+    """generator function that parses fastq or fasta files, yielding
+       (name, sequence, qual) for fastq files, or (name, sequence, None) 
+       for fasta files, for each record
+    """
+
     last = None # this is a buffer keeping the last unprocessed line
+
     while True: # mimic closure; is it a bad idea?
         if not last: # the first record or a record following a fastq
             for l in fp: # search for the start of the next record
+                l = l.rstrip()
                 if l[0] in '>@': # fasta/q header line
-                    last = l[:-1] # save this line
+                    last = l     # save this line
                     break
+                    
         if not last: break
-        name, seqs, last = last[1:].partition(" ")[0], [], None
+
+        name = last[1:]
+        seqs = []
+        last = None
         for l in fp: # read the sequence
+            l = l.rstrip()
             if l[0] in '@+>':
-                last = l[:-1]
+                last = l
                 break
-            seqs.append(l[:-1])
+            seqs.append(l)
+
         if not last or last[0] != '+': # this is a fasta record
             yield name, ''.join(seqs), None # yield a fasta record
             if not last: break
         else: # this is a fastq record
-            seq, leng, seqs = ''.join(seqs), 0, []
+            seq = ''.join(seqs)
+            leng = 0
+            seqs = []
             for l in fp: # read the quality
-                seqs.append(l[:-1])
-                leng += len(l) - 1
+                l = l.rstrip()
+                seqs.append(l)
+                leng += len(l)
                 if leng >= len(seq): # have read enough quality
                     last = None
                     yield name, seq, ''.join(seqs); # yield a fastq record
@@ -36,4 +52,4 @@ if __name__ == "__main__":
         n += 1
         slen += len(seq)
         qlen += qual and len(qual) or 0
-    print n, '\t', slen, '\t', qlen
+    print(n, '\t', slen, '\t', qlen)
